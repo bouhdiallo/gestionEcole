d4f334bc1436f84defd83b44b54b07f14bbb8a2d
Bouhdiallo <bouhdiallo22@gmail.com>
2023-11-14 12:30:08 +0000

    merge

--- database/migrations/2023_11_13_104000_create_notes_table.php
+++ database/migrations/2023_11_13_104000_create_notes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('matiere');
             $table->float('note');
-            $table->string('matiere');
+     
             $table->timestamps();
         });
     }

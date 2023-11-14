3b800d0adc0267e04a8a1656700719867f6ccd4d
Babyd12 <mamadcir24@gmail.com>
2023-11-14 09:31:51 +0000

    Aucune autre mise à jour, juste git qui fais son difficile parce que j'ai supprimer quelque espaces

--- routes/web.php
+++ routes/web.php
@@ -14,21 +14,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('master');
-});
+// Route::get('/', function () {
+//     return view('master');
+// });
+
+
 
 
 Route::get('/eleves/list', [EleveController::class, 'index'])->name('listEleve');
 Route::get('/eleves/add', [EleveController::class, 'create'])->name('addEleve');
-
-
 Route::post('/eleves/store', [EleveController::class, 'store']);
 Route::get('/eleves/show', [EleveController::class, 'store'])->name('show');
 Route::get('/eleves/edit/{id}', [EleveController::class, 'edit']);
-
 Route::post('/eleves/update', [EleveController::class, 'update']);
 Route::delete('/eleves/delete', [EleveController::class, 'destroy']);
+
+// Route::prefix('/note')->name('note.')->controller(EleveController::class)->group(function ()  {
+//     Route::get('/list','indexs')->name('lis');
+//     Route::get('add',  'create')->name('add');
+//     Route::post('/store', 'store')->name('store');
+//     Route::get('/edit/{id}', 'edit');
+//     Route::post('/update',  'update');
+//     Route::delete('/delete','destroy');
+
+// }) ->where([
+//     'id' => [0-9],
+// ]);
+
+
+
+
 
 
 
